23e33d9edeb722e3b5c74c2791afe4bb97728c69
Fabio Galdo <fabiogaldo@gmail.com>
2025-02-28 10:07:36 -0300

    feat: add analytics plugin

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
@@ -10,5 +11,6 @@ const queryClient = new QueryClient();
 root.render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <Analytics mode="production" />
   </QueryClientProvider>
 );
